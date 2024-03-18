ca4c55d1df2e86e6c99123ad4345e75c797e3e95
이원준 <evenil0206@gmail.com>
2024-03-18 23:54:38 +0900

    JPA and H2 dbms + Entity, Tables, Hibernate

--- src/main/java/com/mysite/sbb/HelloController.java
+++ src/main/java/com/mysite/sbb/HelloController.java
@@ -3,7 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Controller // 브라우저의 요청을 처리한다.
 public class HelloController {
 	@GetMapping("/hello") // http://localhost:8080/hello 메소드와 url을 매핑한다. GET 방식 처리
 	@ResponseBody
---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,37 @@
+package com.mysite.sbb;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter // 일반적인 경우 엔티티 클래스에 Setter는 안전하지 않다.
+@Entity // 엔티티로 만든다.
+public class Question {
+	@Id // 이 속성을 기본키로 한다.
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 자동으로 1씩 증가하며 저장됨. 독립적으로 이 속성만 1씩 증가
+	private Integer id;
+	
+	@Column(length = 200)
+	private String subject;
+	
+	@Column(columnDefinition = "TEXT")
+	private String content;
+	
+	private LocalDateTime createDate;
+	
+	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+	private List<Answer> answerList;
+}
