39d35f0fb3a819bdae2d2d729f68575072196ecb
이원준 <evenil0206@gmail.com>
2024-03-22 13:33:20 +0900

    24-03-22 답변 등록, 답변 서비스, 답변컨트롤러, 템플릿 변경, 부트스트랩적용

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -12,18 +12,18 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class QuestionService {
 
-	private final QuestionRepository questionRepository;
+	private final QuestionRepository questionRepository;// 생성자(lombok)을 이용한 의존성 주입.
 	
-	public List<Question> getList() {
+	public List<Question> getList() { // 질문 리스트를 반환하는 서비스 메소드
 		return this.questionRepository.findAll();
 	}
 	
-	public Question getQuestion(Integer id) {
+	public Question getQuestion(Integer id) { // 특정 아이디의 질문을 반환하는 서비스 메소드
 		Optional<Question> question = this.questionRepository.findById(id);
 		if(question.isPresent()) {
-			return question.get();
+			return question.get(); // 나중에 DTO를 반환하도록 수정할 것.
 		} else {
-			throw new DataNotFoundException("question not found");
+			throw new DataNotFoundException("question not found"); // 질문이 없을 경우 발생하는 사용자정의 예의
 		}
 	}
 }
